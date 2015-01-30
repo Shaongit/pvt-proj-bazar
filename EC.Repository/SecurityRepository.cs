3a8187459f616815c673f7ecf80f900b401a3721
mahedee <mahedee.hasan@gmail.com>
2015-01-30 18:04:05 +0600

    Added user repository

---
+++ EC.Repository/SecurityRepository.cs
@@ -0,0 +1,37 @@
+﻿using EC.Context;
+using EC.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC.Repository
+{
+    public class SecurityRepository
+    {
+        ECommerceContext context = new ECommerceContext();
+
+        public SecurityRepository()
+            : this(new ECommerceContext())
+        {
+
+        }
+
+        public SecurityRepository(ECommerceContext context)
+        {
+            this.context = context;
+        }
+
+
+        public UserProfile GetUser(string username)
+        {
+            return this.context.UserProfiles.Where(Usr => Usr.UserName == username).FirstOrDefault();
+        }
+
+        //public IQueryable<Vendor> All
+        //{
+        //    get { return context.Vendors; }
+        //}
+    }
+}
