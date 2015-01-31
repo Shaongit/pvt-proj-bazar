c6e20de8ae226a0e61fbb0ff63fdf17ee4a821d0
mahedee <mahedee.hasan@gmail.com>
2015-01-31 09:56:50 +0600

    Commit by Mahedee

---
+++ EC.BLL/WishItemManager.cs
@@ -0,0 +1,26 @@
+﻿using EC.Model;
+using EC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC.BLL
+{
+    public class WishItemManager
+    {
+        private IGenericRepository<WishItem> repository = null;
+
+        public WishItemManager()
+        {
+            this.repository = new GenericRepository<WishItem>();
+        }
+
+
+        public List<WishItem> GetUserWishItems(int userId)
+        {
+            return repository.SelectAll().Where(p => p.UserId == userId).ToList();
+        }
+    }
+}
